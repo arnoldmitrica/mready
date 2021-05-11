8872b2ee92894597917b5398090467b4351fa831
arnoldmitrica <arnoldmitrica@gmail.com>
2021-05-11 22:28:14 +0300

    Customization

--- mready/Model/RepositoryCellViewModel.swift
+++ mready/Model/RepositoryCellViewModel.swift
@@ -7,14 +7,14 @@
 
 import Foundation
 
-struct RepositoryCellViewModel {
+class RepositoryCellViewModel {
     let full_name:String
     init(with model: Item){
         self.full_name = model.fullName
     }
 }
 
-struct RepositoryDetailViewModel {
+class RepositoryDetailViewModel {
     let fullName:String
     let itemDescription:String
     let language:String?
